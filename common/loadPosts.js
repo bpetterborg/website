0e8a9f176b0aa20d634ab7318ffec7fb054a8d7a
bpetterborg <ben.petterborg@gmail.com>
2021-12-27 13:57:47 -0500

    Cleaned stuff up

--- common/loadPosts.js
+++ common/loadPosts.js
@@ -1,34 +1,32 @@
 /*
- *	Load the sidebar
- *	TODO: Add something to detect if the page is not the index page
- * 		  and change this links to be correct for that page
+ *	Load posts to main page
+ *	TODO:
+ *		- Move thumbnailPaths to a config file
+ * 
  */
 
 // thank you baao from stackoverflow
 
-
-// var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-
 var xhr = new XMLHttpRequest();
 
 // thumbnails to display
-const thumbnailDirs = [
+const thumbnailPaths = [
 	"/posts/2021s1-exam/thumbnail.html",
 	"/posts/apt-reception/thumbnail.html",
 	"/posts/homelab/thumbnail.html"
 ];
 
 // for every thumbnail in array, load it to the appropriate div
-for (let i = 0; i < thumbnailDirs.length; i++)
+for (let i = 0; i < thumbnailPaths.length; i++)
 {
 	xhr.onreadystatechange = function() 
 	{
 		if (xhr.readyState == 4 && xhr.status == 200)
 		{ 
-			document.getElementById( "post" + i ).innerHTML = xhr.responseText;
+			document.getElementById("post" + i).innerHTML = xhr.responseText;
 		} 
 	}
-	xhr.open('get', thumbnailDirs[i], false);
+	xhr.open('get', thumbnailPaths[i], false);
 	xhr.send();
 }
 
