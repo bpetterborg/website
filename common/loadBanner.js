6edfe84c968ee2ca0ad0c8c345d9f90c22891d88
bpetterborg <ben.petterborg@gmail.com>
2022-03-24 13:32:55 -0500

    Add banners

--- common/loadBanner.js
+++ common/loadBanner.js
@@ -1,14 +1,14 @@
-// load banners
+// // load banners
 
-var xhr = new XMLHttpRequest();
+// var xhr = new XMLHttpRequest();
 
-xhr.onreadystatechange = function() 
-{
-    if (xhr.readyState == 4 && xhr.status == 200)
-	{ 
-        document.getElementById("banner").innerHTML = xhr.responseText;
-    } 
-}
+// xhr.onreadystatechange = function() 
+// {
+//     if (xhr.readyState == 4 && xhr.status == 200)
+// 	{ 
+//         document.getElementById("banner").innerHTML = xhr.responseText;
+//     } 
+// }
 
-xhr.open('get', "/html/banner.html", false);
-xhr.send();
+// xhr.open('get', "/html/banner.html", false);
+// xhr.send();
--- common/loadSidebar.js
+++ common/loadSidebar.js
@@ -1,13 +1,6 @@
-/*
- *	Load the sidebar
- *	TODO: Add something to detect if the page is not the index page
- * 		  and change this links to be correct for that page
- */
+// load sidebar
 
-// thank you baao from stackoverflow
 var xhr = new XMLHttpRequest();
-var sidebarPath;
-
 
 xhr.onreadystatechange = function() 
 {
@@ -17,15 +10,5 @@ xhr.onreadystatechange = function()
     } 
 }
 
-// use this if sidebars are different for each page
-if (window.location.pathname == "/") 
-{
-    sidebarPath = "/html/sidebar.html"
-} 
-else 
-{
-    sidebarPath = "/html/sidebar.html"
-}
-
-xhr.open('get', sidebarPath, false);
+xhr.open('get', "/html/sidebar.html", false);
 xhr.send();
